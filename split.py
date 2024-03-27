05afc31859a8fa4ac45d01edf5fb1092c678ed57
00000000014B32D5 <62055039+adam-nadeem7@users.noreply.github.com>
2024-03-28 03:35:39 +0500

    Paper unshredder

---
+++ split.py
@@ -0,0 +1,19 @@
+from PIL import Image
+import os
+
+def imgcrop(input, xPieces, yPieces):
+    filename, file_extension = os.path.splitext(input)
+    im = Image.open(input)
+    imgwidth, imgheight = im.size
+    height = imgheight // yPieces
+    width = imgwidth // xPieces
+    for i in range(0, yPieces):
+        for j in range(0, xPieces):
+            box = (j * width, i * height, (j + 1) * width, (i + 1) * height)
+            a = im.crop(box)
+            try:
+                a.save(filename + "-" + str(j) + file_extension)
+            except:
+                pass
+
+imgcrop("split.jpg", 4, 1)
